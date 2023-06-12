864b98a1bb353f8eddef1c21cb64c78420b6fc18
phliopox <91457591+phliopox@users.noreply.github.com>
2023-06-12 16:23:14 +0900

    .

--- app/src/main/java/com/ian/fastcam/chap3/UnitConversion.kt
+++ app/src/main/java/com/ian/fastcam/chap3/UnitConversion.kt
@@ -17,7 +17,7 @@ class UnitConversion : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = Chap3Binding.inflate(inflater,container,false)
         return binding.root
     }
